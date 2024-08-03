0aa89714fba906b84350a7326e99ae7f514be770
@AlexandruDonoaga <alexandru.donoaga@student.griffith.ie>
2024-08-03 19:42:48 +0100

    fixing scroll bug

--- nindex.js
+++ nindex.js
@@ -1,36 +1,42 @@
+// Toggle menu button
 $(".menu-toggle-btn").click(function () {
   $(this).toggleClass("fa-times");
   $(".navigation-menu").toggleClass("active");
 });
+
 var page = 1,
     moving = false;
 var animationIteration = "animationiteration webkitAnimationIteration mozAnimationIteration oAnimationIteration oanimationiteration",
   transitionEnd      = "transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd";
-$(".load-more").on("click", function() {
-  $("html, body").animate({ scrollTop: 1200 }, 1600,'swing');
-  if ( moving == false ) {
-    moving = true;
-    $(".load-more").addClass("active");
-    setTimeout(function() {
-      $(".load-more").one(animationIteration, function() {
-        $(".load-more").removeClass("active");
-        $(".load-more").one(transitionEnd, function() {
-          page++;
-          moving = false;
-        });
-      });
 
-    }, 2000);
+// Load more button click event
+$(".load-more").on("click", function() {
+  if (moving === false) {
+    moving = true;
+    $("html, body").animate({ scrollTop: "+=1200px" }, 1600, 'swing', function() {
+      $(".load-more").addClass("active");
+      setTimeout(function() {
+        $(".load-more").one(animationIteration, function() {
+          $(".load-more").removeClass("active");
+          $(".load-more").one(transitionEnd, function() {
+            page++;
+            moving = false;
+          });
+        });
+      }, 2000);
+    });
   }
 });
+
 document.addEventListener('DOMContentLoaded', function () {
+  // Intersection Observer
   const observerOptions = {
     root: null,
     rootMargin: '0px',
     threshold: 0.5
   };
 
-  const observer = new IntersectionObserver((entries, observer) => {
+  const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('visible');
@@ -42,9 +48,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const target = document.querySelector('.demolitionText');
   observer.observe(target);
-});
 
-document.addEventListener('DOMContentLoaded', () => {
+  // Pop-up text animation
   const popUpText = document.querySelector('.pop-up-text');
   popUpText.classList.add('active');
 });
